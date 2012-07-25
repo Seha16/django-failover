40f65b69ec2075832c15cbe67255b4df99c34c60
Brian Jay Stanley <brianjaystanley@gmail.com>
2012-07-25 15:03:48 -0400

    added documentation

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ version = "0.1.0"
 
 setup(name="django-failover",
       version=version,
-      description="failover app for Django",
+      description="automatic failover for Django",
       long_description=open("README.rst").read(),
       classifiers=[
           "Development Status :: 4 - Beta",
